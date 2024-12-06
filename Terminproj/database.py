c7ba7dec3b8e6e396ba525741ee998d080b75945
aemrcd <154060630+aemrcd@users.noreply.github.com>
2024-12-06 11:21:58 +0100

    added something for XAAMP

--- Terminproj/database.py
+++ Terminproj/database.py
@@ -4,11 +4,18 @@ from mysql.connector import Error
 def connect_to_database():
     try:
         # Establish connection
+# connection = mysql.connector.connect(
+#     host="10.2.2.29", 
+#     user="Aerol",
+#     password="Anmea050*",
+#     database="Cartdata",
+#     port=3306
+# )
         connection = mysql.connector.connect(
-            host="10.2.2.29", 
-            user="Aerol",
-            password="Anmea050*",
-            database="Cartdata",
+            host="localhost", 
+            user="root",
+            password="",
+            database="cartdata",
             port=3306
         )
         if connection.is_connected():
